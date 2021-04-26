f9e725fcf80d49f70820e248a87212d880946e0e
Gabriel Galperin <galperin@alunos.utfpr.edu.br>
2021-04-26 00:59:44 -0300

    pipeline, gridsearch and fit addition

--- exercise_02_sentiment.py
+++ exercise_02_sentiment.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     # the training data folder must be passed as first argument
     movie_reviews_data_folder = r"./data"
     dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print("n_samples: %d" % len(dataset.data))
+    #print("n_samples: %d" % len(dataset.data))
     #print(dataset.data)
 
     # split the dataset in training and test set:
